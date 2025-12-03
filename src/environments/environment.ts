d7db9b7c38b3af4e8a7c99457a1f728d66a8ccfc
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2025-12-03 11:24:14 -0300

    FIX: fix the url path environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000/api',            
+  baseUrl: 'https://nest-transex-api.onrender.com/api',            
   mapboxKey: "pk.eyJ1IjoiZWNobmVsYXBwNyIsImEiOiJjbWdpNzFuaGIwNjlyMm9wcGZyZ2t1MHN4In0.g9XTWbt31n2bnhrXAUfvzA"
 };
