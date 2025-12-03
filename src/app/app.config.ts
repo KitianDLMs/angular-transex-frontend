6c5c70aa47b1f06c6f000b333c50fc443834e9c8
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2025-12-03 14:05:20 -0300

    FIX: fix routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
+
 import {
   provideHttpClient,
   withFetch,
   withInterceptors,
 } from '@angular/common/http';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
+    provideAnimations(),
     provideHttpClient(
       withFetch(),
       withInterceptors([
