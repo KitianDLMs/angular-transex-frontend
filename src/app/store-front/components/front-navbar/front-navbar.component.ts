d3ef3daac83644beb74a3bb8bef2f9d89fb6e4ec
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2025-12-03 12:37:44 -0300

    FIX: fix routes

--- src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'front-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './front-navbar.component.html',
 })
 export class FrontNavbarComponent {
