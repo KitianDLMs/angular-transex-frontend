e1310c46ba2fbf4ffaf4b6a4515f4ca6c16c4308
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2026-01-27 01:08:19 -0300

    ADD: add ordr module

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // baseUrl: 'http://localhost:3000/api',
-  baseUrl: 'https://nest-transex-api.onrender.com/api',
+  baseUrl: 'https://nest-transex-api.onrender.com/api', 
   mapboxKey: "pk.eyJ1IjoiZWNobmVsYXBwNyIsImEiOiJjbWdpNzFuaGIwNjlyMm9wcGZyZ2t1MHN4In0.g9XTWbt31n2bnhrXAUfvzA"
 };
