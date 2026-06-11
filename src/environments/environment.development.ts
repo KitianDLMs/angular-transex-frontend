fa06a944c52ba12b31fec2ad33c1d92f3aadbf97
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2026-06-11 18:23:44 -0400

    FIX: fix the url change to aws url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   // baseUrl: 'http://localhost:3000/api',
-  baseUrl: 'https://nest-transex-api.onrender.com/api',
+  // baseUrl: 'https://nest-transex-api.onrender.com/api',
+  baseUrl: 'http://18.118.131.163/api',
   mapboxKey: "pk.eyJ1IjoiZWNobmVsYXBwNyIsImEiOiJjbWdpNzFuaGIwNjlyMm9wcGZyZ2t1MHN4In0.g9XTWbt31n2bnhrXAUfvzA"
 };
