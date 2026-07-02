8570304672299d61822eb94e0d02890fb212c216
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2026-07-02 14:33:05 -0400

    FIX: fix the env variables to api references

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // baseUrl: 'http://localhost:3000/api',
   // baseUrl: 'https://nest-transex-api.onrender.com/api',
-  baseUrl: 'http://18.118.131.163:80/api',
+  baseUrl: 'http://10.100.5.117:3000/api',
   mapboxKey: "pk.eyJ1IjoiZWNobmVsYXBwNyIsImEiOiJjbWdpNzFuaGIwNjlyMm9wcGZyZ2t1MHN4In0.g9XTWbt31n2bnhrXAUfvzA"
 };
