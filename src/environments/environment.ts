f044e3ac823e4dee40b294d67141d35598c76539
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2026-07-08 10:25:43 -0400

    FIX: fix the production url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // baseUrl: 'https://nest-transex-api.onrender.com/api',
-  baseUrl: 'http://10.100.5.117:3000/api',
+  baseUrl: 'http://portalcliente-alb-prod-1982395826.us-east-1.elb.amazonaws.com/api',
   mapboxKey: "pk.eyJ1IjoiZWNobmVsYXBwNyIsImEiOiJjbWdpNzFuaGIwNjlyMm9wcGZyZ2t1MHN4In0.g9XTWbt31n2bnhrXAUfvzA"
 };
