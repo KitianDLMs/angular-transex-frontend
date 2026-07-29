13e462e32d3aadcea437ef97042773f2ff5d28f0
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2026-07-29 16:12:46 -0400

    FIX: fix production API URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // baseUrl: 'http://localhost:3000/api',
-  baseUrl: 'http://portalcliente-alb-prod-1982395826.us-east-1.elb.amazonaws.com/api',
+  baseUrl: 'http://localhost:3000/api',
+  // baseUrl: 'http://portalcliente-alb-prod-1982395826.us-east-1.elb.amazonaws.com/api',
   mapboxKey: "pk.eyJ1IjoiZWNobmVsYXBwNyIsImEiOiJjbWdpNzFuaGIwNjlyMm9wcGZyZ2t1MHN4In0.g9XTWbt31n2bnhrXAUfvzA"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // baseUrl: 'https://nest-transex-api.onrender.com/api',
-  baseUrl: 'http://portalcliente-alb-prod-1982395826.us-east-1.elb.amazonaws.com/api',
+  baseUrl: 'https://portalcliente.grupotx.cl/api',
   mapboxKey: "pk.eyJ1IjoiZWNobmVsYXBwNyIsImEiOiJjbWdpNzFuaGIwNjlyMm9wcGZyZ2t1MHN4In0.g9XTWbt31n2bnhrXAUfvzA"
 };
